ac303855bef3f3bb08fc778ec1160070fa7a7012
teodor.nilseng@gmail.com <teodor.nilseng@gmail.com>
2023-02-24 22:08:03 +0100

    feat: initial server setup

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import path from "path";
+import * as url from "url";
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
+
+const app = express();
+
+app.use(express.static(path.join(__dirname)));
+app.use("/lib", express.static(path.join(__dirname, "../lib")));
+app.use("/", express.static(path.join(__dirname, "index.html")));
+
+export const startServer = () => {
+  const serverPromise = new Promise((resolve) => {
+    const port = process.env.PORT || 4000;
+    app.listen({ port }, () => {
+      console.log(`Server listening on port ${port}`);
+      resolve(app);
+    });
+  });
+  return serverPromise;
+};
